726bf50137f9546acf8c92fc30f7f9a56452ca78
Ilya Sharonov <ilia.sharonov@gmail.com>
2016-12-06 23:08:30 +0300

    Added events, updated docs for it.

--- src/common/constants/profile-constants.js
+++ src/common/constants/profile-constants.js
@@ -1,5 +1,5 @@
 /**
- * STATE constant contains all available Profile states:
+ * STATE object contains available Profile states:
  * - INVALID: profile was created / updated, but dependencies were not met.
  * - VALID: profile was created / updated, all dependencies met.
  * @type {Object}
@@ -9,7 +9,15 @@ export const STATE = {
   VALID: 'PROFILE_STATE_VALID',
 };
 
+/**
+ * EVENT object contains available event identifiers for a Profile:
+ * - INVALIDATE: emitted when previously met dependency is invalidated and Profile can no longer be used.
+ * - COMPILE: emitted when Profile requests compilation due to changes in dependencies or Profile itself.
+ * - UPDATED: emitted when a Profile is updated / rcompiled.
+ * @type {Object}
+ */
 export const EVENT = {
   INVALIDATE: 'PROFILE_EVENT_INVALIDATE',
   COMPILE: 'PROFILE_EVENT_COMPILE',
+  UPDATED: 'PROFILE_EVENT_UPDATED',
 };
